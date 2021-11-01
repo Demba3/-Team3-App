6f88ddc7bde66c1a44fed3a9be2b2b483664fed0
Johnny <johnny.casares.7@gmail.com>
2021-11-01 18:33:29 -0400

    Updated app id and clientKey

--- app/src/main/java/com/example/instagram/ParseApplication.java
+++ app/src/main/java/com/example/instagram/ParseApplication.java
@@ -16,8 +16,8 @@ public class ParseApplication extends Application {
         // set applicationId, and server server based on the values in the back4app settings.
         // any network interceptors must be added with the Configuration Builder given this syntax
         Parse.initialize(new Parse.Configuration.Builder(this)
-                .applicationId("L8EiXOh74pueNjBZg1oIRaLIZ5wVWGAKf9MRr2N5")// should correspond to Application Id env variable
-                .clientKey("6jdVkYpow479P25ttqCa0qi79z9orVyRJddP6iha") // should correspond to Client key env variable
+                .applicationId("qZs7SmFwMlDM1Vam0uh1B0NqpbVobSPQNu8bE4bC")// should correspond to Application Id env variable
+                .clientKey("Eiswl2Q6zQdcU7Y2rcg5XmH9Vdr3KDlbq8cHVs73") // should correspond to Client key env variable
                 .server("https://parseapi.back4app.com").build()
         );
     }
